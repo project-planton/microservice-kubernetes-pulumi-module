b83e9f5db8d695293a0853d619e2004853a6c95a
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-08-11 12:26:48 +0530

    replaced istio gateway and virtual-service resources with gateway-apis counterparts

--- pkg/variables.go
+++ pkg/variables.go
@@ -1,12 +1,14 @@
 package pkg
 
 var vars = struct {
+	GatewayIngressClassName    string
 	IstioIngressNamespace      string
 	IstioIngressSelectorLabels map[string]string
 	MainPyConfigMapName        string
 	LibFilesConfigMapName      string
 }{
-	IstioIngressNamespace: "istio-ingress",
+	GatewayIngressClassName: "istio",
+	IstioIngressNamespace:   "istio-ingress",
 	IstioIngressSelectorLabels: map[string]string{
 		"app":   "gateway",
 		"istio": "gateway",
