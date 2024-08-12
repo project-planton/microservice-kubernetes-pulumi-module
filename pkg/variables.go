8540e1aaf969475f4f2548cb58d6ec26cba9b0bd
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-08-12 20:53:37 +0530

    using existing istio lb services for new gateways

--- pkg/variables.go
+++ pkg/variables.go
@@ -1,18 +1,17 @@
 package pkg
 
 var vars = struct {
-	GatewayIngressClassName    string
-	IstioIngressNamespace      string
-	IstioIngressSelectorLabels map[string]string
-	MainPyConfigMapName        string
-	LibFilesConfigMapName      string
+	GatewayIngressClassName                    string
+	GatewayInternalLoadBalancerServiceHostname string
+	GatewayExternalLoadBalancerServiceHostname string
+	IstioIngressNamespace                      string
+	MainPyConfigMapName                        string
+	LibFilesConfigMapName                      string
 }{
-	GatewayIngressClassName: "istio",
-	IstioIngressNamespace:   "istio-ingress",
-	IstioIngressSelectorLabels: map[string]string{
-		"app":   "gateway",
-		"istio": "gateway",
-	},
-	MainPyConfigMapName:   "main-py",
-	LibFilesConfigMapName: "lib-files",
+	GatewayIngressClassName:                    "istio",
+	GatewayInternalLoadBalancerServiceHostname: "ingress-internal.istio-ingress.svc.cluster.local",
+	GatewayExternalLoadBalancerServiceHostname: "ingress-external.istio-ingress.svc.cluster.local",
+	IstioIngressNamespace:                      "istio-ingress",
+	MainPyConfigMapName:                        "main-py",
+	LibFilesConfigMapName:                      "lib-files",
 }
